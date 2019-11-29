ead1b69101abce48a39669c0ef68eceae514aa10
edA-qa mort-ora-y <edaqa@eversystems.eu>
2019-11-29 18:05:27 +0100

    tracking results

--- mysqlripper/__main__.py
+++ mysqlripper/__main__.py
@@ -1,3 +1,5 @@
+from typing import *
+
 import MySQLdb
 import asyncio, sys
 
@@ -31,16 +33,46 @@ def list_ordered_tables():
 	
 	return sorted_tables
 
-async def backup_table(name : str):
-	cmd = f'mysqldump --defaults-file=/longtmp/temp-mysql-pushcoin/mysql/my.cnf sbtest --user=root --password="rootpass" --result-file=/tmp/{name}.sql {name}'
-	proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+async def backup_tables(names : List[str]):
+	all_cmds = [
+		f'mysqldump --defaults-file=/longtmp/temp-mysql-pushcoin/mysql/my.cnf sbtest --user=root --password="rootpass" --result-file=/tmp/{name}.sql {name}' for name in names
+	]
 	
-	stdout, stderr = await proc.communicate()
-	print(f'[{cmd!r} exited with {proc.returncode}]')
+	done = []
+	pending = {}
+	proc_count = 4
+	cmd_at = 0
+	while True:
+		# add tasks as there is space
+		while len(pending) < proc_count and cmd_at < len(all_cmds):
+			cmd = all_cmds[cmd_at]
+			proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+			key = proc.communicate()
+			pending[key] = (proc, cmd)
+			cmd_at += 1
+			
+		# done?
+		if len(pending) == 0:
+			return
+			
+		done, _ = await asyncio.wait({k for k in pending.keys()}, return_when = asyncio.FIRST_COMPLETED )
+		for d in done:
+			result = d.result()
+			# this is often incorrectdly  None when using FIRST_COMPLETED, so we can't reliably use it
+				
+			proc, cmd = pending[d._coro]
+			if proc.returncode != 0:
+				print( d.exception() )
+				raise Exception( "Failed command", cmd )
+
+			#print( "Completed", cmd )
+			del pending[d._coro]
+	
+		#stdout, stderr = 
+		#print(f'[{cmd!r} exited with {proc.returncode}]')
 	
 def main():
 	sorted_tables = list_ordered_tables()
-	for table in sorted_tables:
-		loop.run_until_complete(backup_table(table[0]))
+	loop.run_until_complete( backup_tables( [table[0] for table in sorted_tables] ) )
 	
 main()
